9f0e97aaca38f5d535db10aca249e49c8172ad96
chengzhi <hnyangcz@126.com>
2020-06-22 23:28:01 +0800

    互斥锁-解决原子性问题

---
+++ concurrent-first/src/main/java/com/concurrent/first/X.java
@@ -0,0 +1,19 @@
+package com.concurrent.first;
+
+public class X {
+    // 修饰非静态方法
+    synchronized void foo() {
+        // 临界区
+    }
+    // 修饰静态方法
+    synchronized static void bar() {
+        // 临界区
+    }
+    // 修饰代码块
+    Object obj = new Object();
+    void baz() {
+        synchronized(obj) {
+            // 临界区
+        }
+    }
+}
